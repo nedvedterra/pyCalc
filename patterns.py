ff35909b84e9702ae672fb2bcec23ca7361a5801
Alexander Nedvedski <zevs.olimp.555@gmail.com>
2016-12-16 09:04:55 +0300

    Add types of tokens and corresponding regexp patterns
    - Create the types.py file with types of tokens
    - Create the patterns.py file with the PATTERNS list of regexp patterns

---
+++ patterns.py
@@ -0,0 +1,7 @@
+from . import types
+PATTERNS = [(types.WHITESPACE, r'\s+'),
+            (types.FLOAT, r'(\d*\.){1}\d+\b'),
+            (types.INTEGER, r'\d+\b'),
+            (types.IDENTIFIER, r'[a-zA-Z]+\d*\b'),
+            (types.INTEGERDIVISION, r'\\'),
+            (types.OPERATION, r'[\+\-\*\/\%\^\(\)]')] 
